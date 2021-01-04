ffb6bcccfd30816ffa364b2512d69a9f4cd4805a
bstee615 <benjaminjsteenhoek@gmail.com>
2021-01-03 18:55:28 -0600

    Update deps

--- install-deps.sh
+++ install-deps.sh
@@ -2,3 +2,6 @@
 
 # Install all dependencies for ManyBugs listed in deps.txt (except comments)
 sudo dnf install -y $(grep -v '^#' deps.txt)
+
+# Install glibconfig.h
+sudo cp /usr/lib64/glib-2.0/include/glibconfig.h /usr/include/glib-2.0/glibconfig.h
