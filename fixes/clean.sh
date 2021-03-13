beafffdb9856d21d0d46cf8739698b6dc12836d1
benjis <benjis@iastate.edu>
2021-03-12 21:36:16 -0600

    Add global clean script

---
+++ fixes/clean.sh
@@ -0,0 +1,23 @@
+rm -rf fixed-program.txt \
+    *.cache \
+    *~ \
+    compile.pl \
+    coverage.* \
+    coverage \
+    configuration-*
+    sanity \
+    preprocessed \
+    *debug* \
+    tests \
+    local-root \
+    fixed \
+    .Indicator_Makefiles \
+    repair.debug.0
+mv fix-failures bug-info
+mv bug-failures bug-info
+mv fix.lines bug-info
+mv fault.lines bug-info
+mv bugged-program.txt bug-info
+
+cp test.sh.back test.sh
+sed -i.back "s@/root/mountpoint-genprog/genprog-many-bugs/$(cat bug-info/original-name)/@$(cat bug-info/original-name)@g" test.sh
