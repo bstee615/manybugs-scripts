52c10cec2c133bac8afb51138ad2b38b4f303225
benjis <benjis@iastate.edu>
2021-03-12 21:36:52 -0600

    Add multiple dl

---
+++ activate.sh
@@ -0,0 +1,7 @@
+bugs_root=$(dirname $0)
+
+PATH="$bugs_root/scripts:$PATH"
+export PATH
+
+PYTHONPATH="$bugs_root/scripts:$PYTHONPATH"
+export PYTHONPATH
---
+++ scripts/dl_multiple.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from pathlib import Path
+from dl import download
+import sys
+import fnmatch
+
+"""
+Download multiple scenarios according to a regex
+"""
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        raise Exception(f'Usage: {sys.argv[0]} <regex-to-download>')
+
+    for prefix in sys.argv[1:]:
+        df = pd.read_csv(Path(__file__).parent / 'bug-data.csv', header=0)
+        to_download = df[df['scenario name'].str.match(prefix)]['scenario name']
+        assert len(to_download) > 0, 'No scenarios matched.'
+        for bugname in to_download:
+            download(bugname)
