0eab047a09ebc745fb8d43dc074efc34d7c0c0f9
benjis <benjis@iastate.edu>
2021-03-15 15:38:31 -0500

    Add and unify deps

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install all dependencies for ManyBugs listed in deps.txt (except comments)
-sudo dnf install -y $(grep -v '^#' deps.txt)
+sudo dnf install -y $(grep -v '^#' $(dirname $0)/deps.txt)
 
 # Install glibconfig.h
 sudo cp /usr/lib64/glib-2.0/include/glibconfig.h /usr/include/glib-2.0/glibconfig.h
