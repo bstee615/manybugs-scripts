f949fa0e8cde150310a6870a7cceb11b535e8589
bstee615 <benjis@iastate.edu>
2021-03-23 00:25:31 -0400

    Make activate.sh robust to filepaths it's given

--- activate.sh
+++ activate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-bugs_root=$(dirname ${BASH_SOURCE[0]})
+bugs_root=$(realpath $(dirname ${BASH_SOURCE[0]}))
 
 PATH="$bugs_root/scripts:$PATH"
+PATH="$bugs_root/fixes:$PATH"
 export PATH
 
 PYTHONPATH="$bugs_root/scripts:$PYTHONPATH"
